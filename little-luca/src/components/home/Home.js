7449b9aa10f2420462de28f4de67389d0bdeae69
Ben <bigriffin94@outlook.com>
2020-02-14 14:19:21 -0800

    initialize Project

---
+++ little-luca/src/components/home/Home.js
@@ -0,0 +1,37 @@
+/*
+
+MAIN
+- show Menu Button
+- show catering button
+- show famous sauce button
+- since 1980 image
+
+Nav bar
+- order online
+- about us
+- jobs
+- location(s)
+
+Footer
+- Facebook!!
+- order online
+- about us
+- jobs
+- location(s)
+- menu
+- email list
+- copyright "Copyright © 2004,2009,2013,2015,2017 Little Lucca Specialty Sandwich Shop, All Rights Reserved."
+*/
+
+import React from './node_modules/react';
+
+function Home() {
+    return (
+        // HOME WRAPPER
+        <div>
+            Hello From the home page!!
+        </div>
+    )
+}
+
+export default Home;
---
+++ little-luca/src/components/navBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function NavBar(){
+    return (
+        <div>
+            <nav>
+            This will be the nav bar
+            </nav>
+        </div>
+    )
+}
