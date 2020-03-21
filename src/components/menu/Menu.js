28b894b7a55588658c429962e857cf41ab488ee1
Ben <bigriffin94@outlook.com>
2020-03-21 15:55:18 -0700

    Menu styling is clean

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -7,17 +7,21 @@ import './menu.css'
 
 function Menu() {
     return (
-        <div className="menu-wrapper">
-            <a href={menupdf} download="little-luca-menu">
-                <div className="menu-img-wrapper">
-                <img src={menu} alt="PDF MENU"/>
-                </div>
-            </a>
-            <p
-                className="menu-para"
-            >
-                Please click on the menu if you would like to download the image or you may click/tap and hold to save to your mobile device. 
-            </p>
+        <div>
+            <div
+                className="menu-wrapper"
+            > 
+                <a href={menupdf} download="little-luca-menu">
+                    <div className="menu-img-wrapper">
+                    <img src={menu} alt="PDF MENU"/>
+                    </div>
+                </a>
+                <p
+                    className="menu-para"
+                >
+                    Please click on the menu if you would like to download the image or you may click/tap and hold to save to your mobile device. 
+                </p>
+            </div>
         </div>
     )
 }
